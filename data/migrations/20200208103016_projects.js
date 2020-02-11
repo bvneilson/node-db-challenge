28bb3b2f5071c3d198cfebb15692bee63eebadce
Brennan Neilson <bvneilson@gmail.com>
2020-02-10 19:34:04 -0700

    Adding bridge table

--- data/migrations/20200208103016_projects.js
+++ data/migrations/20200208103016_projects.js
@@ -28,11 +28,25 @@ exports.up = function(knex) {
         .unique()
         .notNullable();
       tbl.text('description', 128);
+    })
+    .createTable('project_resources', tbl => {
+      tbl.integer('project_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('projects');
+      tbl.integer('resource_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('resources');
+      tbl.primary(['project_id', 'resource_id']);
     });
 };
 
 exports.down = function(knex) {
   return knex.schema
+    .dropTableIfExists('project_resources')
     .dropTableIfExists('resources')
     .dropTableIfExists('tasks')
     .dropTableIfExists('projects');
